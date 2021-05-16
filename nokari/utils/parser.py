98c35f5072f4b1aa5a6bd4681e0b95dc2130c5f2
Norizon <norizontunes@gmail.com>
2021-05-17 04:35:28 +0700

    Improve parser and add more comments

--- nokari/utils/parser.py
+++ nokari/utils/parser.py
@@ -3,17 +3,21 @@ from typing import (
     TYPE_CHECKING,
     Any,
     Coroutine,
+    ClassVar,
     Dict,
     Final,
     List,
     Literal,
+    Tuple,
     Optional,
     Sequence,
     TypedDict,
     Union,
 )
 
-from nokari.utils.view import StringView, UnexpectedQuoteError
+from lightbulb.utils import find
+
+from nokari.utils.view import StringView, UnexpectedQuoteError, _quotes
 
 if TYPE_CHECKING:
     from nokari.core.context import Context
@@ -24,7 +28,7 @@ __all__: Final[List[str]] = [
 ]
 
 
-class _Parser:
+class Cursor:
     def __init__(self, parser: "ArgumentParser", argument: str):
         self.parser = parser
         self.view = StringView(argument or "")
@@ -72,12 +76,14 @@ class ArgumentOptions(TypedDict, total=False):
 
 
 class ArgumentParser:
+    invalid_openings: ClassVar[Tuple[str, ...]] = tuple(f"{i}-" for i in _quotes)
+
     __slots__ = (
         "params",
         "force",
         "replace",
         "valid_names",
-        "short_flags",
+        "short_keys",
         "_default_key",
         "_default_name",
     )
@@ -107,7 +113,7 @@ class ArgumentParser:
         self.force = force
         self.replace = replace
         self.valid_names = set()
-        self.short_flags = set()
+        self.short_keys = set()
         self._default_key = append_remainder_to
 
         for k, v in self.params.items():
@@ -122,53 +128,92 @@ class ArgumentParser:
             for alias in self.params[k]["aliases"]:
                 self.valid_names.add(alias)
 
-            if v.get("argmax") == 0 and k != v["name"]:
-                self.short_flags.add(k)
+            if k != v["name"]:
+                self.short_keys.add(k)
 
         self._default_name = (
             params[self._default_key]["name"] if self._default_key else None
         )
 
     # pylint: disable=too-many-branches
-    def process_word(
-        self, parser: _Parser, argument: str, *, allow_sf: bool = True
+    def switch_cursor(
+        self, cursor: Cursor, argument: str, *, allow_sf: bool = True
     ) -> bool:
         key = argument.lstrip("-").lower()
-        data = parser.data
+        data = cursor.data
         params = self.params
         names = self.valid_names
         temp_dict: Optional[ArgumentOptions] = ArgumentOptions(name=None)
         if argument[0] == "-" and argument[1] != "-":
+            # it's a short flag / option
             if (temp_dict := params.get(key)) and temp_dict["name"] != key:
+                # if it's a short flag, set it to True and return
+                # otherwise, continue
                 if temp_dict.get("argmax", -1) == 0:
                     data[temp_dict["name"]] = True
                     return False
+            # if the short flag / option equals the long one
+            # then it's invalid, since it's a long flag / option only
+            # therefore, it should start with double dash
+            # so set the temp cursor to the remainder (None is remainder)
             elif temp_dict and temp_dict["name"] == key:
-                temp_dict = dict(name=None)
-            elif allow_sf and any(i for i in self.short_flags if i in key):
-                for f in self.short_flags:
-                    if f not in argument:
+                temp_dict = ArgumentOptions(name=None)
+            # it's a multiple short flags
+            # or a short option but the argument sticks to the option,
+            # e.g., -mnorizon instead of -m norizon
+            elif allow_sf and find(self.short_keys, lambda i: i in argument):
+                argument = argument[1:]
+                has_flags = False
+                while argument and (
+                    short_key := find(
+                        self.short_keys,
+                        lambda i: i.startswith(argument[0]) and len(i) <= len(argument),
+                    )
+                ):
+                    name = params[short_key]["name"]
+                    argument = argument[len(short_key) :]
+
+                    # it's a valid short flag
+                    # continue to look for more short flags
+                    if params[short_key].get("argmax") == 0:
+                        data[name] = True
+                        has_flags = True
                         continue
 
-                    argument = argument.replace(f, "", 1)
-                    data[params[f]["name"]] = True
+                    # it's a valid short option
+                    # append the argument and return
+                    if not has_flags:
+                        if isinstance((lst := data.get(name)), list):
+                            lst.append(argument)
+                        else:
+                            data[name] = [argument]
 
-                argument = argument[1:]
+                        return False
+
+                # argument is exhausted. So, return
                 if not argument:
                     return False
 
-                temp_dict = params.get(argument)
+                # argument isn't exhausted
+                # so, switch the temp cursor to the remainder
+                temp_dict = ArgumentOptions(name=None)
 
         elif argument.startswith("--") and argument[2] != "-":
             if key in names:
                 for v in params.values():
+                    # it's a valid long key
                     if key == v["name"] or key in v["aliases"]:
                         temp_dict = v
                         break
 
+        # no valid key was found
+        # switch the temp cursor to the remainder!
         if temp_dict is None:
             temp_dict = ArgumentOptions(name=None)
 
+        # no valid key was found and we want to append it to the remainder
+        # or it's actually a valid key, but it's duplicated
+        # and we want to append it to the remainder instead
         if (temp_dict["name"] is None and not self.force) or (
             (temp_dict["name"] in data and not self.replace)
         ):
@@ -176,20 +221,25 @@ class ArgumentParser:
                 lst.append(argument)
             return False
 
-        parser.current_dict, parser.current_key = temp_dict, temp_dict["name"]
-        if parser.current_key != self._default_name:
-            data[parser.current_key] = []
+        # set the current cursor and initialize the list
+        cursor.current_dict, cursor.current_key = temp_dict, temp_dict["name"]
+        if cursor.current_key != self._default_name:
+            data[cursor.current_key] = []
 
+        # return True to notify that we successfully switched the cursor
         return True
 
-    def append(self, parser: _Parser, argument: str) -> None:
-        max_length = parser.current_dict.get("argmax", -1)
-        data = parser.data[parser.current_key]
+    def append(self, cursor: Cursor, argument: str) -> None:
+        # append arguments to the current cursor
+        max_length = cursor.current_dict.get("argmax", -1)
+        data = cursor.data[cursor.current_key]
         if isinstance(data, list):
             data.append(argument)
             count = len(data)
+            # if the current cursor can't receive more arguments
+            # we set the current cursor to the remainder
             if max_length != -1 and count >= max_length:
-                parser.current_key = self._default_name
+                cursor.current_key = self._default_name
 
     def convert(
         self, ctx: "Context", argument: str
@@ -201,14 +251,20 @@ class ArgumentParser:
         There's no reason for this method to be async.
         But it might be useful in the future.
         """
-        parser = _Parser(self, argument)
-        view = parser.view
+        cursor = Cursor(self, argument)
+        view = cursor.view
 
         while not view.eof:
+            # we only wanna skip space, not other whitespaces
             view.skip_char(" ")
 
-            pass_ = view.buffer[view.index : view.index + 2] == '"-'
+            # we wanna skip it if it startswith "-
+            # it'll be valid since we're gonna strip the quotes
+            pass_ = view.buffer.startswith(self.invalid_openings)
 
+            # no"rizon" by default is invalid
+            # we're gonna make it valid here
+            # it'll be parsed as norizon
             try:
                 index = view.index
                 argument = view.get_quoted_word() or ""
@@ -219,11 +275,14 @@ class ArgumentParser:
 
             temp_argument = argument.strip()
 
+            # it's not invalid openings
+            # and at least is assumed as valid short key
             if (
                 pass_ is False
                 and temp_argument.startswith("-")
                 and len(temp_argument) != 1
             ):
+                # it's in --key=argument or -k=argument format
                 if (
                     "=" in temp_argument
                     and (arguments := temp_argument.split("="))
@@ -231,13 +290,17 @@ class ArgumentParser:
                 ):
                     key, *arguments = arguments
                     argument = "=".join(arguments)
-                    if self.process_word(parser, key, allow_sf=False):
-                        self.append(parser, argument)
+                    if self.switch_cursor(cursor, key, allow_sf=False):
+                        # the cursor isn't at remainder
+                        # therefore, it's a valid key
+                        # so, append the argument to the cursor
+                        self.append(cursor, argument)
                         continue
 
-                self.process_word(parser, temp_argument)
+                self.switch_cursor(cursor, temp_argument)
                 continue
 
-            self.append(parser, argument)
+            # append every argument to the current cursor
+            self.append(cursor, argument)
 
-        return parser.finish()
+        return cursor.finish()
