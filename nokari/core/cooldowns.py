571097d32fe7c8ea3c22f47c6c1b0adebd035629
Norizon <norizontunes@gmail.com>
2021-05-31 22:22:28 +0700

    Better custom cooldown

--- nokari/core/cooldowns.py
+++ nokari/core/cooldowns.py
@@ -3,59 +3,32 @@
 import time
 import typing
 
-from lightbulb import Bucket, CooldownManager, CooldownStatus, commands, errors
+from lightbulb import Bucket, commands, cooldowns
 
 from .context import Context
 
 __all__: typing.Final[typing.List[str]] = ["cooldown"]
 
 
-class CustomCooldown(CooldownManager):
-    """Custom CooldownManager class with alternative hashes."""
-
-    def __init__(
-        self, *args: typing.Union[float, Bucket], **kwargs: typing.Any
-    ) -> None:
-        super().__init__(*args)
-        self.elements: typing.Sequence[int] = kwargs.get(
-            "elements", [265080794911866881]
-        )
-        self.alter_length: int = kwargs.get("alter_length", 0)
-        self.alter_usages: int = kwargs.get("alter_usages", 1)
-
-    def add_cooldown(self, context: Context) -> None:
-        cooldown_hash = self.bucket.extract_hash(context)
-        cooldown_bucket = self.cooldowns.get(cooldown_hash)
-        if cooldown_bucket is not None:
-            cooldown_status = cooldown_bucket.acquire()
-            if cooldown_status == CooldownStatus.ACTIVE:
-                raise errors.CommandIsOnCooldown(
-                    "This command is on cooldown",
-                    command=context.command,
-                    retry_in=(cooldown_bucket.start_time + cooldown_bucket.length)
-                    - time.perf_counter(),
-                )
-            if cooldown_status == CooldownStatus.INACTIVE:
-                return
-        if cooldown_hash in self.elements:
-            self.cooldowns[cooldown_hash] = self.bucket(
-                self.alter_length, self.alter_usages
-            )
-        else:
-            self.cooldowns[cooldown_hash] = self.bucket(self.length, self.usages)
-        self.cooldowns[cooldown_hash].acquire()
-
-
 def cooldown(
     length: float,
     usages: int,
     bucket: Bucket,
-    **kwargs: typing.Union[int, typing.Sequence[int]],
+    elements: typing.Optional[typing.Sequence[int]] = None,
+    alter_length: float = 0,
+    alter_usages: int = 1,
 ) -> typing.Callable[[commands.Command], commands.Command]:
     """Returns a decorator that applies customized cooldown to a Command object."""
 
-    def decorate(command: commands.Command) -> commands.Command:
-        command.cooldown_manager = CustomCooldown(length, usages, bucket, **kwargs)
-        return command
+    def callback(context: Context) -> Bucket:
+        nonlocal elements
+        if not elements:
+            elements = [265080794911866881]
 
-    return decorate
+        cd_hash = bucket.extract_hash(context)
+        if cd_hash in elements:
+            return bucket(alter_length, alter_usages)
+
+        return bucket(length, usages)
+
+    return cooldowns.cooldown(callback=callback)
