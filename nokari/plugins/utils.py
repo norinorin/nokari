028d864936413e580277980505d6c8974966ec5c
Norizon <norizontunes@gmail.com>
2021-07-10 21:11:52 +0700

    Bug fixes

--- nokari/plugins/utils.py
+++ nokari/plugins/utils.py
@@ -18,7 +18,7 @@ from nokari.core.context import Context
 from nokari.utils import db, plural, timers
 from nokari.utils.chunker import chunk, simple_chunk
 from nokari.utils.converters import time_converter
-from nokari.utils.formatter import escape_markdown, human_timedelta
+from nokari.utils.formatter import escape_markdown, human_timedelta, discord_timestamp
 from nokari.utils.paginator import Paginator
 from nokari.utils.parser import ArgumentParser
 
@@ -123,7 +123,9 @@ class Utils(Plugin):
             interval=interval,
         )
         delta = (when - now).total_seconds()
-        if delta <= 60:
+
+        # Only optimise non-interval short timers
+        if delta <= 60 and not interval:
             asyncio.create_task(self.short_timer_optimisation(delta, timer))
             return timer
 
@@ -161,14 +163,14 @@ class Utils(Plugin):
         """
         You can pass a human readable time. The argument order doesn't really matter here,
         but you can't pass the time in between the reminder message. The time should be in UTC.
-        Examples: - `n!remind me in a week do something`
-        - `n!remind me at 2pm do something`
-        - `n!remind do something 3h`
-        - `n!remind me in 4 hours do something -i`
-        - `n!remind --daily 4am do something.`
+        Examples: - `n!remind me in a week do something`;
+        - `n!remind me at 2pm do something`;
+        - `n!remind do something 3h`;
+        - `n!remind me in 4 hours do something -i`;
+        - `n!remind --daily 4am do something.`.
 
         Flags:
-        -i, --interval: continuously remind you at set interval.
+        -i, --interval: continuously remind you at set interval;
         -d, --daily: same as interval with 24 hours period.
         """
         parsed = self._remind_parser.parse(None, when)
@@ -200,25 +202,27 @@ class Utils(Plugin):
             message_id=ctx.message.id,
             interval=interval,
         )
-        reminder_id = f"Reminder ID: {timer.id}" if timer.id else ""
-        fmt = "<t:{timestamp}:F>"
-        pre = "on"
+        reminder_id = f" Reminder ID: {timer.id}" if timer.id else ""
+        fmt = "R"
+        pre = ""
 
         if parsed.interval:
             reminder_id += f" with interval {human_timedelta(timedelta(seconds=typing.cast(int, interval)))}"
         elif parsed.daily:
-            fmt = "<t:{timestamp}:t>"
-            pre = "at"
-            reminder_id = f"Daily {reminder_id}"
+            fmt = "t"
+            pre = "at "
+            reminder_id = f"Daily{reminder_id}"
 
         await ctx.respond(
-            f"{ctx.author.mention}, {reminder_id} {pre} {fmt.format(timestamp=int(timer.expires_at.timestamp()))}: {rem}"
+            f"{ctx.author.mention},{reminder_id} {pre}"
+            f"{discord_timestamp(timer.expires_at, fmt=fmt)}: {rem}"
+            f"{'.'*(not rem.endswith('.'))}"
         )
 
     @remind.command(name="list")
     async def remind_list(self, ctx: Context) -> None:
         """
-        Shows your current active remainders...
+        Shows your current active remainders.
         """
 
         def get_embed(
@@ -298,7 +302,7 @@ class Utils(Plugin):
 
         has_guild = hasattr(channel, "guild_id")
         if (msg_id := event.timer.kwargs.get("message_id")) and has_guild:
-            message += f'\n\n[Jump URL](https://discordapp.com/channels/{channel.guild_id}/{channel.id}/{msg_id} "Jump to the message")'
+            message += f'\n\n[Jump URL](https://discordapp.com/channels/{channel.guild_id}/{channel.id}/{msg_id} "Jump to the message.")'
 
         embed = (
             hikari.Embed(
@@ -308,7 +312,7 @@ class Utils(Plugin):
             .add_field(name="ID:", value=str(event.timer.id))
             .add_field(
                 name="Created on:",
-                value=f"<t:{int(event.timer.created_at.timestamp())}:F>",
+                value=discord_timestamp(event.timer.created_at, fmt="F"),
             )
         )
 
@@ -361,7 +365,7 @@ class Utils(Plugin):
 
         query = "DELETE FROM reminders WHERE event = 'reminder' AND extra #>> '{args,1}' = $1"
         await self.bot.pool.execute(query, author_id)
-        await ctx.respond(f"Your {plural(len(records)):reminder} has been deleted")
+        await ctx.respond(f"Your {plural(len(records)):reminder} has been deleted.")
 
     # pylint: disable=too-many-locals
     @remind.command(name="info", usage="<ID>")
@@ -374,7 +378,7 @@ class Utils(Plugin):
         Example: `n!remind info 378 --raw`
 
         Flags:
-        -r, --raw: escape the markdown in the message
+        -r, --raw: escape the markdown in the message.
         """
         parsed = (
             ArgumentParser()
@@ -395,30 +399,40 @@ class Utils(Plugin):
                    AND id = $2
                 """
 
-        def format_dt(dt: datetime) -> str:
-            return f"<t:{int(dt.timestamp())}:F>"
-
         record = await self.bot.pool.fetchrow(
             query, str(ctx.author.id), parsed.remainder
         )
         if not record:
             await ctx.respond(f"You have no reminder with ID: {parsed.remainder}.")
 
+        interval = record["interval"]
         extra = record["extra"]
         channel_id, author_id, message = extra["args"]
         converter = escape_markdown if parsed.raw else lambda s: s
         embed = (
             hikari.Embed(title=f"Reminder no. {parsed.remainder}")
             .add_field(name="Message:", value=converter(message))
-            .add_field(name="Created on:", value=format_dt(record["created_at"]))
-            .add_field(name="Expires on:", value=format_dt(record["expires_at"]))
-            .add_field(name="Expires in:", value=human_timedelta(record["expires_at"]))
+            .add_field(
+                name="Created on:",
+                value=discord_timestamp(record["created_at"], fmt="F"),
+            )
         )
 
-        if record["interval"]:
+        if not interval:
+            embed.add_field(
+                name="Expires on:",
+                value=discord_timestamp(record["expires_at"], fmt="F"),
+            )
+
+        embed.add_field(
+            name=f"{'Next one' if interval else 'Expires'} in:",
+            value=f"{human_timedelta(record['expires_at'])}",
+        )
+
+        if interval:
             embed.add_field(
                 name="Interval:",
-                value=human_timedelta(timedelta(seconds=record["interval"])),
+                value=human_timedelta(timedelta(seconds=interval)),
             )
 
         channel = (
@@ -431,7 +445,7 @@ class Utils(Plugin):
         if (msg_id := extra["kwargs"].get("message_id")) and has_guild:
             embed.add_field(
                 name="Jump URL",
-                value=f'[Click here](https://discordapp.com/channels/{channel.guild_id}/{channel.id}/{msg_id} "Jump to the message")',
+                value=f'[Click here](https://discordapp.com/channels/{channel.guild_id}/{channel.id}/{msg_id} "Jump to the message.")',
             )
 
         await ctx.respond(embed=embed)
@@ -445,10 +459,10 @@ class Utils(Plugin):
     async def remind_delete(self, ctx: Context, ids: Greedy[int] = None) -> None:
         """
         This command can be greedy and take more than 1 ID.
-        Example: `n!remind delete 243 244` with 243 and 244 as the IDs
+        Example: `n!remind delete 243 244` with 243 and 244 as the IDs.
         """
         if not ids:
-            await ctx.respond("Please specify the ID(s) of the reminder")
+            await ctx.respond("Please specify the ID(s) of the reminder.")
             return
 
         author_id = str(ctx.author.id)
@@ -477,7 +491,7 @@ class Utils(Plugin):
         have = "have" if plural_ else "has"
 
         await ctx.respond(
-            f"{reminder} with id {', '.join(map(str, deletes))} {have} been deleted"
+            f"{reminder} with id {', '.join(map(str, deletes))} {have} been deleted."
         )
 
     @remind.command(
@@ -504,7 +518,7 @@ class Utils(Plugin):
         if self._current_timer and self._current_timer.id == id_:
             self._current_timer.args[2] = message  # type: ignore
 
-        await ctx.respond(f"Alright, it's now set to {message}")
+        await ctx.respond(f"Alright, it's now set to {message}.")
 
 
 def load(bot: Bot) -> None:
