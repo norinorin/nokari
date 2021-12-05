ab01df189cf08ccf23c16adbe3edcac46ef553b8
Norizon <69400842+norinorin@users.noreply.github.com>
2021-12-05 20:16:13 +0700

    Pluralization

--- kita/__init__.py
+++ kita/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Norizon"
 __version__ = "SNAPSHOT"
 __license__ = "MPL-2.0"
 
-from kita.command_handler import *
+from kita.command_handlers import *
 from kita.commands import *
 from kita.data import *
 from kita.options import *
